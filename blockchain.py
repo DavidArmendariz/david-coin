322026326a8a8f28f535615768c7e80ed701124d
David Armendariz <darmendariz1998@outlook.com>
2021-07-18 13:34:59 -0500

    Added add_node method

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import datetime
 import hashlib
 import json
+from urllib.parse import urlparse
 
 
 class Blockchain:
@@ -8,6 +9,7 @@ class Blockchain:
         self.chain = []
         self.transactions = []
         self.create_block(proof=1, previous_hash="0")
+        self.nodes = set()
 
     def create_block(self, proof, previous_hash):
         block = {
@@ -65,3 +67,7 @@ class Blockchain:
         )
         previous_block = self.get_previous_block()
         return previous_block["index"] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
